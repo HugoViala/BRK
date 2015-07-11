b3b6c1777fa6abeb8a16f28ca81da21e582c199d
HugoViala <hugo.viala@gmail.com>
2015-07-10 19:05:43 -0800

    Basic SDL Setup

--- src/brk.cpp
+++ src/brk.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
+#include <SDL.h>
 
-int main()
+#undef main
+
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello, world!" << std::endl;
+    if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0)
+    {
+	// TODO(hugo): We can't load SDL. There is an issue.
+	return(0);
+    }
+
+    SDL_Window* window = SDL_CreateWindow("BRK",
+					  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+					  256, 512, SDL_WINDOW_SHOWN);
+    if(window)
+    {
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+    }
+    else
+    {
+    }
+   
+
+    SDL_Quit();
     return(0);
 }
