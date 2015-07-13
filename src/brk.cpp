7de640fbd89302d64b15acbd2eb666f8769a534a
HugoViala <hugo.viala@gmail.com>
2015-07-12 21:11:53 -0800

    Rect drawing and keyboard event

--- src/brk.cpp
+++ src/brk.cpp
@@ -15,15 +15,15 @@ int main(int argc, char** argv)
 					  SDL_WINDOWPOS_CENTERED,
 					  SDL_WINDOWPOS_CENTERED,
 					  500, 546, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);;
-    SDL_Surface* background = SDL_LoadBMP("../data/duck.bmp");
-    
-    SDL_RenderClear(renderer);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, background);
-    if(texture)
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+						SDL_RENDERER_ACCELERATED);
 
+    SDL_Rect r;
+    r.x = 50;
+    r.y = 450;
+    r.w = 100;
+    r.h = 25;
+    
     bool gameContinue = true;
     
     while(gameContinue)
@@ -36,11 +36,24 @@ int main(int argc, char** argv)
 	    {
 		gameContinue = false;
 	    }
+	    if(event.type == SDL_KEYDOWN)
+	    {
+		if(event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+		    r.x -= 4;
+		if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+		    r.x += 4;
+	    }
 	}
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	    
+	SDL_RenderClear(renderer);
+	
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(renderer, &r);
+    
+	SDL_RenderPresent(renderer);
     }
     
-    SDL_FreeSurface(background);
-    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
