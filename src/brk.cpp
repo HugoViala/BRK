91919cec538c45a58f11b8d8f29f32b552ccc3c9
HugoViala <hugo.viala@gmail.com>
2015-07-12 21:29:38 -0800

    Basic collision for the paddle

--- src/brk.cpp
+++ src/brk.cpp
@@ -1,6 +1,14 @@
 #include <SDL.h>
-#include <iostream>
+
 #undef main
+
+/*
+  TODO(hugo):
+  - Smooth the speed of the paddle
+  - Detection collision for the paddle (see that it does not get away from the screen)
+  - Add a ball to play around with
+  - Support Gamepad
+ */
 
 
 int main(int argc, char** argv)
@@ -11,51 +19,55 @@ int main(int argc, char** argv)
 	return(0);
     }
 
-    SDL_Window* window = SDL_CreateWindow("BRK",
+    int WindowWidth = 500;
+    int WindowHeight = 546;
+    SDL_Window* Window = SDL_CreateWindow("BRK",
 					  SDL_WINDOWPOS_CENTERED,
 					  SDL_WINDOWPOS_CENTERED,
-					  500, 546, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+					  WindowWidth, WindowHeight, 0);
+    SDL_Renderer* Renderer = SDL_CreateRenderer(Window, -1,
 						SDL_RENDERER_ACCELERATED);
 
-    SDL_Rect r;
-    r.x = 50;
-    r.y = 450;
-    r.w = 100;
-    r.h = 25;
+    SDL_Rect Paddle;
+    Paddle.x = 50;
+    Paddle.y = 450;
+    Paddle.w = 100;
+    Paddle.h = 25;
     
-    bool gameContinue = true;
+    bool GameContinue = true;
     
-    while(gameContinue)
-	
+    while(GameContinue)	
     {
-	SDL_Event event;
-	while(SDL_PollEvent(&event))
+	SDL_Event Event;
+	while(SDL_PollEvent(&Event))
 	{
-	    if(event.type == SDL_QUIT)
+	    if(Event.type == SDL_QUIT)
 	    {
-		gameContinue = false;
+		GameContinue = false;
 	    }
-	    if(event.type == SDL_KEYDOWN)
+	    if(Event.type == SDL_KEYDOWN)
 	    {
-		if(event.key.keysym.scancode == SDL_SCANCODE_LEFT)
-		    r.x -= 4;
-		if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
-		    r.x += 4;
+		if(Event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+		{
+		    if(Paddle.x - 4 >= 0)
+			Paddle.x -= 4;
+		}
+		if(Event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+		    if(Paddle.x + Paddle.w + 4 <= WindowWidth)
+			Paddle.x += 4;
+		}
 	    }
 	}
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	    
-	SDL_RenderClear(renderer);
-	
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderFillRect(renderer, &r);
-    
-	SDL_RenderPresent(renderer);
+	SDL_SetRenderDrawColor(Renderer, 128, 128, 128, 255);	    
+	SDL_RenderClear(Renderer);	
+	SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(Renderer, &Paddle);
+	SDL_RenderPresent(Renderer);
     }
     
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(Renderer);
+    SDL_DestroyWindow(Window);
     SDL_Quit();
     return(0);
 }
